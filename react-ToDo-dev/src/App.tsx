adf22da71ad75f990674568e6a8482023c3f2076
takumi.takahashi <takumi.takahashi@gpol.co.jp>
2024-05-24 15:09:42 +0900

    first commit

---
+++ react-ToDo-dev/src/App.tsx
@@ -0,0 +1,27 @@
+import { TodoList } from "./components/TodoList";
+import { AddTodoForm } from "./components/AddTodoForm";
+import { TodoSummary } from "./components/TodoSummary";
+import { useTodoList } from "./hooks/useTodoList";
+
+function App(){
+  const {todoList, changeCompleted, addTodo, deleteTodo, deleteAllCompleted} = useTodoList();
+
+  return (
+    <main className="mx-auto mt-10 max-w-xl">
+      <h1 className="text-center text-4xl">Todoアプリ</h1>
+      <div className="space-7-5">
+        <AddTodoForm addTodo={addTodo} />
+        <div className="space-y-5 rounded bg-slate-200 p-5 mt-5">
+          <TodoList 
+            todoList={todoList} 
+            changeCompleted={changeCompleted}
+            deleteTodo={deleteTodo}
+          />
+          <TodoSummary deleteAllCompleted={deleteAllCompleted} />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
